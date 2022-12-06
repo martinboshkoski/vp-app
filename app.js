0204138e13d6a35b57d5985be9f5f9312851a462
MARTIN BOSHKOSKI <55806675+martinboshkoski@users.noreply.github.com>
2022-12-06 21:34:40 +0100

    connected

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(morgan('combined'))
 
 db.connectToDatabase().then(function(){
     // app.listen(3000);
-    console.log('connecte')
+    console.log('connected')
     app.listen(process.env.PORT || 3000);
 }).catch(function(error){
     console.log('Failed to connect to the database');
