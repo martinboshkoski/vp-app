a39454e57ac7d6ed27ca338f91b17c3a30a00063
MARTIN BOSHKOSKI <55806675+martinboshkoski@users.noreply.github.com>
2022-12-18 10:12:57 +0100

    Update payments.controller.js

--- controllers/payments.controller.js
+++ controllers/payments.controller.js
@@ -1,6 +1,7 @@
 const Payment = require("../models/payment.model");
 const Policy = require("../models/policy.model");
 const Client = require("../models/client.model");
+const Agent = require("../models/agent.model");
 
 const mongodb = require("mongodb");
 const moment = require("moment");
@@ -8,19 +9,43 @@ const db = require("../data/database");
 
 async function getPayments(req, res, next) {
   try {
+    const agent = await Agent.getAgentWithSameId(req.session.uid);
+    const agentName = agent.name;
     const payments = await Payment.findAll();
-    res.render("agents/payment/all-payments", { payments: payments });
+    let sum = 0;
+    let pickedDate = '___/___/______'
+    res.render("agents/payment/all-payments", { payments: payments, agentName:agentName, sum:sum, pickedDate:pickedDate });
   } catch (error) {
     next(error);
     return;
   }
 }
 
-async function getByDate(req, res, next){
+async function getByDate(req, res, next) {
   try {
-    const payments = await Payment.findByDate(req.body.date);
-    res.render("agents/payment/all-payments", { payments: payments });
-  
+    const agent = await Agent.getAgentWithSameId(req.session.uid);
+    const agentName = agent.name;
+
+//da se generiraat samo tie uplati sto se povrzani/primeni od konkretniot
+
+    const payments = await Payment.findByDateAndAgent(req.body.date, agentName);
+
+/// Sum of all payments
+let allPayments = []
+for (payment of payments) {
+  allPayments.push(payment.paymentAmount)
+}
+/////
+let sum = allPayments.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+////
+const pickedDate = moment(req.body.date).format('DD/MM/YYYY')
+
+res.render("agents/payment/all-payments", {
+      payments: payments,
+      agentName:agentName, 
+      sum:sum, 
+      pickedDate:pickedDate
+    });
   } catch (error) {
     next(error);
     return;
@@ -28,58 +53,81 @@ async function getByDate(req, res, next){
 }
 
 async function insertPayment(req, res, next) {
-
   try {
     const policy = await Policy.findById(req.body.policyId);
-    const policyNumber = req.body.policyNumber
-let agentCommision;
-if (policy.policyAmount > +req.body.payment ) {
-  agentCommision = +req.body.payment * 0.15
-}
-if (policy.policyAmount === +req.body.payment ) {
-  agentCommision = +req.body.payment * 0.2
-}
+    const policyNumber = req.body.policyNumber;
+    let agentCommision;
+    if (policy.policyAmount > +req.body.payment) {
+      agentCommision = +req.body.payment * 0.15;
+    }
+    if (policy.policyAmount === +req.body.payment) {
+      agentCommision = +req.body.payment * 0.2;
+    }
 
-const thePayment = {
-  amount: +req.body.payment, 
-  agentCommision: Math.round(agentCommision),
-  agent: req.body.agentName,
-  date: moment().format('YYYY-MM-DD')
-}
+    const thePayment = {
+      amount: +req.body.payment,
+      agentCommision: Math.round(agentCommision),
+      agent: req.body.agentName,
+      date: moment().format("YYYY-MM-DD"),
+    };
 
-await db.getDb().collection("policies").updateOne({ policyNumber: policyNumber }, {$push: {"thePayment": thePayment}});//needs to go into the model 
+    await db
+      .getDb()
+      .collection("policies")
+      .updateOne(
+        { policyNumber: policyNumber },
+        { $push: { thePayment: thePayment } }
+      ); //needs to go into the model
 
     //going to the same client page
     const clientPin = req.body.pin;
-    const client = await Client.findByPin(clientPin)
-    const clientName = client.name
+    const client = await Client.findByPin(clientPin);
+    const clientName = client.name;
 
     //registering payment in the paymentcs collection
-    const payment = new Payment(clientName, clientPin, policy.policyNumber, thePayment.amount, thePayment.agent);
+    const payment = new Payment(
+      clientName,
+      clientPin,
+      policy.policyNumber,
+      thePayment.amount,
+      thePayment.agent
+    );
     try {
-      await payment.save(clientName, clientPin, thePayment.amount, policy.policyNumber, thePayment.agent);
+      await payment.save(
+        clientName,
+        clientPin,
+        thePayment.amount,
+        policy.policyNumber,
+        thePayment.agent
+      );
     } catch (error) {
       next(error);
       return;
     }
 
     //total paid for one policy (result)
-    const allPayments = policy.thePayment
+    const allPayments = policy.thePayment;
     let totalPaymentAmounts = [];
     for (singlePayment of allPayments) {
-      totalPaymentAmounts.push(singlePayment.amount)
-    } 
+      totalPaymentAmounts.push(singlePayment.amount);
+    }
     let totalPaid = totalPaymentAmounts.reduce(function (x, y) {
       return x + y;
-  }, 0);
-  const finalResult = totalPaid + thePayment.amount;
+    }, 0);
+    const finalResult = totalPaid + thePayment.amount;
 
-  await db.getDb().collection("policies").updateOne({ policyNumber: policyNumber }, {$set: {"totalPaid": finalResult}});//needs to go into the model 
+    await db
+      .getDb()
+      .collection("policies")
+      .updateOne(
+        { policyNumber: policyNumber },
+        { $set: { totalPaid: finalResult } }
+      ); //needs to go into the model
 
     const clientThroughPayment = await Payment.findByPin(clientPin.toString());
     const objectId = clientThroughPayment._id;
-    const theClientPage = '/agents/clients/' + objectId
-    res.redirect(theClientPage);  
+    const theClientPage = "/agents/clients/" + objectId;
+    res.redirect(theClientPage);
   } catch (error) {
     next(error);
     return;
@@ -109,6 +157,28 @@ module.exports = {
   getPayments: getPayments,
   insertPayment: insertPayment,
   generatePerDate: generatePerDate,
-  getByDate:getByDate,
+  getByDate: getByDate,
+  // generatePaymentsReport: generatePaymentsReport
   // getByPin: getByPin
 };
+
+// async function generatePaymentsReport(req, res, next) {
+//     const theDate = req.body.date;
+//     try {
+//       let payments = await Payment.findAll();
+//       let todayPayments = [];
+
+//       // for (todayPayment of payments) {
+//       //   if (
+//       //     moment(req.body.date).format("DD/MM/YYYY") == moment(todayPayment.date).format("DD/MM/YYYY")
+//       //   ) {
+//       //     todayPayments.push(todayPayment);
+//       //   }
+//       // }
+
+//     res.render("agents/payment/by-date", { theDate: theDate });
+//   } catch (error) {
+//     next(error);
+//     return;
+//   }
+// }
