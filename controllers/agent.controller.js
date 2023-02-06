eacf928e9bc5c896a9354173f8c960de15732159
MARTIN BOSHKOSKI <55806675+martinboshkoski@users.noreply.github.com>
2023-02-06 22:14:22 +0100

    agentReport/agentController - calculations that were missing (!)

--- controllers/agent.controller.js
+++ controllers/agent.controller.js
@@ -2,7 +2,6 @@ const Client = require("../models/client.model");
 const Agent = require("../models/agent.model");
 const Policy = require("../models/policy.model");
 const Payment = require("../models/payment.model");
-
 const mongodb = require("mongodb");
 const moment = require("moment");
 const db = require("../data/database");
@@ -11,41 +10,51 @@ const db = require("../data/database");
 
 async function getAgent(req, res, next) {
   const agentSeller = req.body.agentSeller;
-  const policiesOfAgent = await Policy.findByAgent(agentSeller);
-
   const startDate = moment(req.body.startDate).format("YYYY-MM-DD");
   const endDate = moment(req.body.endDate).format("YYYY-MM-DD");
+  let policiesOfAgent = await Policy.findByAgent(agentSeller);
 
-  // za uplati po polisi od prethoden period
+  let allPoliciesInPeriod = [];
+  let allPoliciesInPeriodPremium = [];
+
+  //////////// Polisirana premija (polisi) ////////////
+  if (agentSeller === "vp") {
+    policiesOfAgent = await Policy.findAll();
+    for (policy of policiesOfAgent) {
+      const requiredPolicy = moment(policy.policyNumber.policyDate).isBetween(startDate, endDate);
+      if (requiredPolicy) {
+        allPoliciesInPeriod.push(policy);
+        allPoliciesInPeriodPremium.push(policy.policyNumber.policyAmount)
+      }
+    }
+  }
+
+  let totalallPoliciesInPeriodPremium = allPoliciesInPeriodPremium.reduce(function (x, y) {
+    return x + y;
+  }, 0);
+
+//////////// Polisi so naplatena premija ////////////////////////////////////////////////////
   let requiredPoliciesByPayment = [];
   let totalCommission = [];
   let totalPremium = [];
   let totalPaidPremium = [];
 
-  for (policy of policiesOfAgent) {
-
-    // const requiredPolicyBetween = moment(policy.date).isBetween(startDate, endDate);
-    // if(requiredPolicyBetween) {
-    //   requiredPoliciesByPayment.push(policy);
-    //   totalPremium.push(policy.policyAmount);
-    // }
-
-    for (payment of policy.thePayment) {
-      const requiredPolicy = moment(payment.date).isBetween(startDate, endDate); // to filter from the DB for better performance
-
+  if (agentSeller === "vp") {
+    policiesOfAgent = await Policy.findAll();
+    for (policy of policiesOfAgent) {
+      let requiredPolicy = moment(policy.policyNumber.policyDate).isBetween(startDate, endDate);
       if (requiredPolicy) {
-        requiredPoliciesByPayment.push(policy);
-// if (policy.thePayment.length > 1) {
-//   requiredPoliciesByPayment.push(policy);
-// }
-        totalPremium.push(policy.policyAmount);
-        totalPaidPremium.push(policy.totalPaid);
-        const singlePayment = payment.agentCommision;
-        totalCommission.push(singlePayment);
+        for (payment of policy.policyNumber.thePayment) {
+          if (payment.amount !== 0) {
+            requiredPoliciesByPayment.push(policy.policyNumber);
+            totalPremium.push(policy.policyNumber.policyAmount);
+            totalPaidPremium.push(payment.amount);
+            totalCommission.push(payment.agentCommision);
+          }
+        }
       }
     }
   }
-
   //sums of the elements of the arrays:
   let totalAgentCommission = totalCommission.reduce(function (x, y) {
     return x + y;
@@ -58,13 +67,54 @@ async function getAgent(req, res, next) {
   let thetotalPaidPremium = totalPaidPremium.reduce(function (x, y) {
     return x + y;
   }, 0);
+////////////////////////////////////////////////////////////////////////////////
+if (agentSeller !== "vp") {
+  for (policy of policiesOfAgent) {
+    theRequiredAgentPolicy = moment(policy.policyDate).isBetween(startDate, endDate);
+    if (theRequiredAgentPolicy) {
+      allPoliciesInPeriod.push(policy)
+      allPoliciesInPeriodPremium.push(policy.policyAmount)
+    }
+  }
+  totalallPoliciesInPeriodPremium = allPoliciesInPeriodPremium.reduce(function (x, y) {
+    return x + y;
+  }, 0);
+}
+
+if (agentSeller !== "vp") {
+  for (policy of policiesOfAgent) {
+    theRequiredAgentPolicy = moment(policy.policyDate).isBetween(startDate, endDate);
+    if (theRequiredAgentPolicy) {
+      for (payment of policy.thePayment) {
+        console.log(payment)
+        if (payment.amount !== 0) {
+          requiredPoliciesByPayment.push(policy);
+          totalPremium.push(policy.policyAmount);
+          totalPaidPremium.push(payment.amount);
+          totalCommission.push(payment.agentCommision);
+        }
+      }
+    }
+  }
+  totalAgentCommission = totalCommission.reduce(function (x, y) {
+    return x + y;
+  }, 0);
+  theTotalPremium = totalPremium.reduce(function (x, y) {
+    return x + y;
+  }, 0);
+  thetotalPaidPremium = totalPaidPremium.reduce(function (x, y) {
+    return x + y;
+  }, 0);
+}
 
   res.render("admin/agent-report", {
     agentSeller: agentSeller,
     requiredPoliciesByPayment: requiredPoliciesByPayment,
+    allPoliciesInPeriod: allPoliciesInPeriod,
     totalAgentCommission: totalAgentCommission,
     theTotalPremium: theTotalPremium,
     thetotalPaidPremium: thetotalPaidPremium,
+    totalallPoliciesInPeriodPremium: totalallPoliciesInPeriodPremium,
     startDate: startDate,
     endDate: endDate,
     moment: moment,
