dd2f80ee01e85180605500fd3abbc0101f3f26b9
MARTIN BOSHKOSKI <55806675+martinboshkoski@users.noreply.github.com>
2024-02-25 20:42:19 +0100

    payments controller fix

--- controllers/payments.controller.js
+++ controllers/payments.controller.js
@@ -1,194 +1,261 @@
-const Policy = require("../models/policy.model");
 const Payment = require("../models/payment.model");
+const Policy = require("../models/policy.model");
+const Client = require("../models/client.model");
+const Agent = require("../models/agent.model");
+
+const mongodb = require("mongodb");
 const moment = require("moment");
-const db = require('../data/database')
+const db = require("../data/database");
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-async function getPolicies(req, res, next) {
+////////////////////////////////////////////////////////////////////////
+async function getPayments(req, res, next) {
   try {
-    const policies = await Policy.findAll();
-    const policiesNumber = await Policy.countAll();
+    const agent = await Agent.getAgentWithSameId(req.session.uid);
+    const agentName = agent.name;
 
-    //Calculates the total paid amount
-    let paidAmounts = [];
     const payments = await Payment.findAll();
-    for (payment of payments) {
-      paidAmounts.push(payment.paymentAmount);
-    }
-    let totalPaidAmounts = paidAmounts.reduce(function (x, y) {
-      return x + y;
-    }, 0);
+    let sum = 0;
+    let pickedDate = '___/___/______'
+    res.render("agents/payment/all-payments", { payments: payments, agentName:agentName, sum:sum, pickedDate:pickedDate });
+  } catch (error) {
+    next(error);
+    return;
+  }
+}
 
-    //Calculates the total amount of the premium
-    let totalPremium = [];
-    for (policy of policies) {
+////////////////////////////////////////////////////////////////////////
+async function getByDate(req, res, next) {
+  try {
+    const agent = await Agent.getAgentWithSameId(req.session.uid);
+    const inputAgent = req.body.agentName;
+    const agentName = inputAgent || agent.name;
 
-      totalPremium.push(policy.policyNumber.policyAmount);
+    const payments = await Payment.findByDateAndAgent(req.body.date, agentName);
 
-      const policyDate = moment(policy.policyNumber.policyDate);
-      const threeMonthsAgo = moment().subtract(3, 'months');
+/// Sum of all payments
+let allPayments = []
+for (payment of payments) {
+  allPayments.push(+payment.paymentAmount)
+}
+let unformatedSum = (allPayments.reduce((accumulator, currentValue) => accumulator + currentValue, 0))
 
-      const paymentPercentage = (policy.policyNumber.totalPaid || 0) / policy.policyNumber.policyAmount;
-      // Determine policy status based on payment percentage
-      if (paymentPercentage >= 1) {
-          // Policy is fully paid
-          policy.isUnpaid = false;
-          policy.discount = false;
-      } else if (paymentPercentage >= 0.79) {
-          // Policy is paid 80% or more but less than 100%
-          policy.discount = true;
-          policy.isUnpaid = false;
-      } else {
-          // Policy is paid less than 80%
-          policy.isUnpaid = true;
-          policy.discount = false;
-      }
-    }
-    let totalAmountsPremium = totalPremium.reduce(function (x, y) {
-      return x + y;
-    }, 0);
-    
-    const percentagePayment = (
-      (totalPaidAmounts / totalAmountsPremium) *
-      100
-    ).toFixed(2);
+let sum = unformatedSum.toLocaleString('de-DE');
 
-    totalAmountsPremium = totalAmountsPremium.toLocaleString('de-DE');
-    totalPaidAmounts = totalPaidAmounts.toLocaleString('de-DE');
+////
+const pickedDate = moment(req.body.date).format('DD/MM/YYYY')
 
-    res.render("agents/policies/policies", {
-      policies: policies,
-      policiesNumber: policiesNumber,
-      totalAmountsPremium: totalAmountsPremium,
-      totalPaidAmounts: totalPaidAmounts,
-      percentagePayment: percentagePayment,
-      moment:moment
+res.render("agents/payment/by-date", {
+      payments: payments,
+      agentName:agentName, 
+      sum:sum, 
+      pickedDate:pickedDate
     });
   } catch (error) {
     next(error);
     return;
   }
 }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-async function insertNewPolicy(req, res, next) {
-  const clientPin = req.body.pin;
-  const clientName = req.body.name;
-  const policyNumber = req.body.insurancePolicy;
-  const policyType = req.body.insurancePolicyType;
-  const registrationNumber = req.body.registrationNumber;
-  const policyAmount = req.body.amount;
-  const installmentsNumber = req.body.installmentsNumber;
-  const policyDate = req.body.insurancePolicyDate;
-  const agentSeller = req.body.agentSeller;
-  const thePayment = [];
-  
-//Validation: Checking of the policy number already exists 
-  const thePolicyNumber = policyNumber.toString()
-  const policyByNumber = await db.getDb().collection('policies').findOne({policyNumber:thePolicyNumber})
-  if (policyByNumber) {
-    res.status(400).send(`<h1> Веќе постои полиса со број ${policyNumber}. Само врати се назад на back (односно "←" на пребарувачот) и смени го бројот на полиса</h1>`);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+async function generatePerDate(req, res, next) {
+  try {
+    let payments = await Payment.findAll();
+    let todayPayments = [];
+    for (todayPayment of payments) {
+      if (
+        moment(req.body.date).format("MM/DD/YYYY") ==
+        moment(todayPayment.date).format("DD/MM/YYYY")
+      ) {
+        todayPayments.push(todayPayment);
+      }
+    }
+    res.render("agents/payment/all-payments", { payments: payments });
+  } catch (error) {
+    next(error);
     return;
+  }
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+async function insertPayment(req, res, next) {
+  try {
+    const policy = await Policy.findById(req.body.policyId);
+    const policyNumber = req.body.policyNumber;
+
+    let agentCommision;
+    if (policy.policyAmount > +req.body.payment) {
+      agentCommision = +req.body.payment * 0.15;
+    }
+    if (policy.policyAmount === +req.body.payment) {
+      agentCommision = +req.body.payment * 0.20;
     }
 
-  //
-  const policy = new Policy(
-    policyNumber,
-    policyType,
-    registrationNumber,
-    policyAmount,
-    installmentsNumber,
-    policyDate,
-    clientPin, 
-    clientName,
-    agentSeller,
-    thePayment,
-  );
-  try {
-    await policy.save(
-      policyNumber,
-      policyType,
-      policyAmount,
-      installmentsNumber,
-      policyDate,
-      clientPin, 
+    const thePayment = {
+      amount: +req.body.payment,
+      agentCommision: Math.round(agentCommision),
+      agent: req.body.agentName,
+      date: moment().format("YYYY-MM-DD"),
+      paidCash: req.body.paidCash
+    };
+
+    await db
+      .getDb()
+      .collection("policies")
+      .updateOne(
+        { policyNumber: policyNumber },
+        { $push: { thePayment: thePayment } }
+      ); //needs to go into the model
+
+    //going to the same client page
+    const clientPin = req.body.pin;
+    const client = await Client.findByPin(clientPin);
+    const clientName = client.name;
+
+    //registering payment in the paymentcs collection
+    const payment = new Payment(
       clientName,
-      agentSeller,
-      thePayment,
+      clientPin,
+      policy.policyNumber,
+      thePayment.amount,
+      thePayment.agent, 
+      thePayment.paidCash
     );
+    try {
+      await payment.save(
+        clientName,
+        clientPin,
+        thePayment.amount,
+        policy.policyNumber,
+        thePayment.agent,
+        thePayment.paidCash
+      );
+    } catch (error) {
+      next(error);
+      return;
+    }
+
+    //total paid for one policy (result)
+    const allPayments = policy.thePayment;
+    let totalPaymentAmounts = [];
+    for (singlePayment of allPayments) {
+      totalPaymentAmounts.push(singlePayment.amount);
+    }
+    let totalPaid = totalPaymentAmounts.reduce(function (x, y) {
+      return x + y;
+    }, 0);
+    const finalResult = totalPaid + thePayment.amount;
+
+    await db
+      .getDb()
+      .collection("policies")
+      .updateOne(
+        { policyNumber: policyNumber },
+        { $set: { totalPaid: finalResult } }
+      ); //needs to go into the model
+
+    const clientThroughPayment = await Payment.findByPin(clientPin.toString());
+    const objectId = clientThroughPayment._id;
+    const theClientPage = "/agents/clients/" + objectId;
+    res.redirect(theClientPage);
   } catch (error) {
     next(error);
     return;
   }
-
-  //finding the url for the concrete client that has made the payment (to transfer to his/her page)
-  const clientThroughPayment = await Payment.findByPin(clientPin.toString());
-  const objectId = clientThroughPayment._id;
-  const theClientPage = "/agents/clients/" + objectId;
-  res.redirect(theClientPage);
 }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-async function getByDate(req, res, next) {
-const startDate = req.body.startDate
-const endDate = req.body.endDate
-
-const requiredPoliciesByDate = await Policy.findByDate(startDate, endDate)
-
-let totalPolicyAmount = 0
-let totalUnPaidPolicyAmount = 0;
-
-requiredPoliciesByDate.forEach(policy => {
-  const policyDate = moment(policy.policyDate);
-  const threeMonthsAgo = moment().subtract(3, 'months');
-
-  const paymentPercentage = (policy.totalPaid || 0) / policy.policyAmount;
-  // Determine policy status based on payment percentage
-  if (paymentPercentage >= 1) {
-      // Policy is fully paid
-      policy.isUnpaid = false;
-      policy.discount = false;
-  } else if (paymentPercentage >= 0.79) {
-      // Policy is paid 80% or more but less than 100%
-      policy.discount = true;
-      policy.isUnpaid = false;
-  } else {
-      // Policy is paid less than 80%
-      policy.isUnpaid = true;
-      policy.discount = false;
-      totalUnPaidPolicyAmount += policy.policyAmount;
-  }
-    //
-  totalPolicyAmount += policy.policyAmount
-})
-
-res.render("agents/policies/policies-by-date", {
-  requiredPoliciesByDate: requiredPoliciesByDate,
-  startDate: startDate, 
-  endDate: endDate, 
-  moment: moment, 
-  totalPolicyAmount:totalPolicyAmount,
-  totalUnPaidPolicyAmount:totalUnPaidPolicyAmount.toLocaleString("de-DE")
-});
-} 
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-async function deleteSinglePolicy(req, res, next) {
-  const policyNumber = req.body.policyNumber;
-  const deletePolicyId = req.body.policyId;
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+async function insertLawsuitPayment(req, res, next) {
   try {
-    await Policy.removePolicy(deletePolicyId);
-    await Payment.deletePaymentsByPolicyNumber(policyNumber);
-    res.redirect(`/agents/clients/${req.body.clientId}`);
+    const thePayment = {
+      suedClientName: req.body.suedClientName,
+      principal: +req.body.principal,
+      interest: +req.body.interest,
+      costs: +req.body.costs,
+      date: moment().format("DD-MM-YYYY"),
+      paidCash: "paidCash"
+    };
+
+    const agent = await Agent.getAgentWithSameId(req.session.uid);
+    let clientPin = "/"; // Ensure this variable is correctly assigned a value
+    let policyNumber = "/"; // Ensure this variable is correctly assigned a value
+
+    // Create and save principal payment
+    const principalPayment = new Payment(
+      thePayment.suedClientName,
+      clientPin,
+      thePayment.principal, // This assumes you corrected the order of arguments in the constructor
+      "Главен долг (по тужба)",
+      agent.name,
+      thePayment.paidCash
+    );
+    await principalPayment.save();
+
+    // Create and save interest payment
+    const interestPayment = new Payment(
+      thePayment.suedClientName,
+      clientPin,
+      thePayment.interest,
+      "Законска казнена камата (по тужба)",
+      agent.name,
+      thePayment.paidCash
+    );
+    await interestPayment.save();
+
+    // Create and save costs payment
+    const costsPayment = new Payment(
+      thePayment.suedClientName,
+      clientPin,
+      thePayment.costs,
+      "Судски трошоци (по тужба)",
+      agent.name,
+      thePayment.paidCash
+    );
+    await costsPayment.save();
+
+    res.redirect("/all-payments");
   } catch (error) {
     next(error);
+    return;
   }
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+async function editPayment(req, res, next) {
+  const policyId = req.body.policyId;
+  const policyNumber = req.body.policyNumber;
+  const paymentDateOld = req.body.paymentDateOld;
+  const paymentAmountOld = req.body.paymentAmountOld;
+  const paidCash = req.body.paidCash;
+
+  const paymentAmount = req.body.paymentAmount;
+  const paymentDate = req.body.paymentDate;
+
+  try {
+    const payment = await Payment.findPaymentByPolicyNumberAndDate(policyNumber, paymentDateOld);
+    const policy = await Policy.findById(policyId)
+
+    await Payment.updatePayment(payment._id, paymentAmount, paymentDate, paidCash);
+    await Policy.updatePolicyPayment(policyId, paymentDateOld, paymentAmountOld, paymentAmount, paymentDate, paidCash);
+
+    if (!payment) {
+      throw new Error('Payment not found');
+    }
+    //go to the same client page
+    const clientPin = req.body.pin;
+    const clientThroughPayment = await Payment.findByPin(clientPin.toString());
+    const objectId = clientThroughPayment._id;
+    const theClientPage = "/agents/clients/" + objectId;
+    res.redirect(theClientPage);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'An error occurred while editing the payment' });
+  }
+}
+
 module.exports = {
-  getPolicies: getPolicies,
-  insertNewPolicy: insertNewPolicy,
-  getByDate:getByDate, 
-  deleteSinglePolicy:deleteSinglePolicy
+  getPayments: getPayments,
+  insertPayment: insertPayment,
+  insertLawsuitPayment: insertLawsuitPayment,
+  generatePerDate: generatePerDate,
+  getByDate: getByDate,
+  editPayment:editPayment
 };
+
