c6b8fffe6ce8ece094d1dc295b6887307e72ded9
MARTIN BOSHKOSKI <55806675+martinboshkoski@users.noreply.github.com>
2024-09-11 16:45:04 +0200

    back to /clients

--- routes/base.routes.js
+++ routes/base.routes.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router();
 
 router.get('/', function(req, res){
-    res.redirect('/login')
+    res.redirect('/clients')
 })
 
 router.get('/401', function(req, res){
